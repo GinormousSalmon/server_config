dee3b7433451316b1563b411f7236690f497f67e
GinormousSalmon <54933940+GinormousSalmon@users.noreply.github.com>
2020-10-08 04:16:53 +0300

    Create moscuitto_install.sh

---
+++ moscuitto_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# curl "https://raw.githubusercontent.com/GinormousSalmon/server_config/main/setup_hass.sh" | bash -s
+
+apt-add-repository ppa:mosquitto-dev/mosquitto-ppa
+apt-get update
+apt-get upgrade -y
+apt install mosquitto -y
+apt-get install mosquitto-clients -y
+
+echo "allow_anonymous false" >> /etc/mosquitto/mosquitto.conf
+echo "password_file /etc/mosquitto/mosquitto.pwd" >> /etc/mosquitto/mosquitto.conf
+
